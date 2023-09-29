b839cfe4baaa38d4ffcee04a017645936a783525
anandmatkar <139226044+anandmatkar@users.noreply.github.com>
2023-09-29 10:32:24 +0530

    commit-comment

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ import Registeredaccount from './Componets/pages/Admin/Registeredaccount';
 import TechnicianTable from './Componets/pages/Teachnician/assignedProject';
 import ProjectList from './Componets/pages/Teachnician/viewProjectDetails';
 import NavbarManager from './Componets/pages/Manager/Navbar';
-
+import ManageTechnician from './Componets/pages/Manager/ManageTechnician';
+import CreateTechnician from './Componets/pages/Manager/CreateTechnician';
+//function
 function App() {
   const authCtx = useContext(AuthContext);
   return (
