3816edfd83faa90dc28703443444996c23005aad
Vaishnavi Agrawal <vaishnaviagrawal006@gmail.com>
2023-10-05 19:06:17 -0400

    commit - change whole ui of login and dashboard

--- src/Componets/pages/Teachnician/TeachLogin.js
+++ src/Componets/pages/Teachnician/TeachLogin.js
@@ -21,162 +21,79 @@ function TechnicianLogin() {
   const authCtx = useContext(AuthContext);
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
-  // const buttonLoginHandler = () => {
-  //   console.log("button clicked");
-  //   navigate("/register");
-  // };
   const submitHandler = (event) => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
     localStorage.setItem("enteredEmail", JSON.stringify(enteredEmail));
-    // optional: Add validation
     setIsLoading(true);
+    
     if (isLogin) {
-      axios.post("http://localhost:3003/api/v1/technician/techLogin", {
-        email: enteredEmail,
-        password: enteredPassword,
-        returnSecureToken: true,
-      })
-      .then((response) => {
-        setIsLoading(false);
-        console.log(response.data, 'response data');
-        if (response.data.status === 200) {
-          const data = response.data.data;
-          if (data.token) {
-            // Save the token in cookies
-            Cookies.set('token', data.token, { expires: 2 }); // Setting expiry to 2 days
-            authCtx.login(data.token);
-            navigate("/techD");
-          } else {
-            throw new Error("Invalid response from the server");
-          }
-        } else if (response.data.status === 401) {
-          throw new Error("Incorrect email or password. Please try again.");
-        } else {
-          throw new Error("Authentication failed!");
-        }
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        alert(err.message);
-      });
-    } else {
-      axios
-        .post("http://localhost:3003/api/v1/technician/techLogin", {
-          body: JSON.stringify({
+        axios.post("http://localhost:3003/api/v1/technician/techLogin", {
             email: enteredEmail,
             password: enteredPassword,
-            //   confirmPass: enteredConfirmPass,
             returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
         })
-        .then((res) => {
-          setIsLoading(false);
-          console.log(res);
-          if (res.ok) {
-            return res.json();
-          } else if (res.status === 401) {
-            throw new Error("Incorrect password. Please try again.");
-          } else {
-            return res.json().then((data) => {
-              let errorMessage = "Authentication failed!";
-              if (data && data.error && data.error.message) {
-                errorMessage = data.error.message;
-              }
-              throw new Error(errorMessage);
-            });
-          }
-        })
-        .then((data) => {
-          const expireTokentime = new Date(
-            new Date().getTime() + +data.expiresIn * 60 * 60
-          );
-          authCtx.login(data.idToken, expireTokentime.toISOString());
-          navigate("/techD");
+        .then((response) => {
+            setIsLoading(false);
+            console.log(response.data, 'response data');
+            if (response.data.status === 200) {
+                const data = response.data.data;
+                if (data.token) {
+                    Cookies.set('token', data.token, { expires: 2 });
+                    authCtx.login(data.token);
+                    navigate("/techD");
+                } else {
+                    throw new Error("Invalid response from the server");
+                }
+            } else if (response.data.status === 401) {
+                throw new Error("Incorrect email or password. Please try again.");
+            } else {
+                throw new Error("Authentication failed!");
+            }
         })
         .catch((err) => {
-          alert(err.message);
+            setIsLoading(false);
+            alert(err.message);
         });
-    }
-  };
+    } // end of isLogin check
+}; 
   return (
     <LayoutTech>
-      <div style={style}>
-      <div class="container h-100">
-    <div class="row h-100 justify-content-center align-items-center">
-        <form class="col-md-9">
-            <div class="AppForm shadow-lg">
-                <div class="row">
-                    <div class="col-md-6 d-flex justify-content-center align-items-center">
-                        <div class="AppFormLeft">
-                            <h1>Technician Login</h1>
-                            <div className="form-outline mb-4">
-                            <label className="form-label" htmlFor="form2Example17">
-                                    Email address
-                                </label>
-                             
-                                <input
-                                    type="email"
-                                    id="form2Example17"
-                                    className="form-control form-control-lg border-top-0 border-right-0 border-left-0 rounded-0 shadow-none"
-                                    placeholder="Email address"
-                                    ref={emailInputRef}
-                                />
-                            </div>
-                            <div className="form-outline mb-4">
-                            <label className="form-label" htmlFor="form2Example27">
-                                    Password
-                                </label>
-                                
-                                <input
-                                    type="password"
-                                    id="form2Example27"
-                                    className="form-control form-control-lg border-top-0 border-right-0 border-left-0 rounded-0 shadow-none"
-                                    placeholder="Password"
-                                    ref={passwordInputRef}
-                                />
-                            </div>
-                            <div class="row mt-4 mb-4">
-                                <div class="col-md-6">
-                                    <div class="form-check">
-                                        <label class="form-check-label" for="defaultCheck1">
-                                            Remember me
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 text-right">
-                               <Link to="/TechnicianForgotPassword" className="small text-muted">
-                                     Forgot password?
-                               </Link>
-                                </div>
-                            </div>
-                            <div className="pt-1 mb-4">
-                                <button
-                                    className="btn btn-success btn-block shadow border-0 py-2 text-uppercase"
-                                    type="button"
-                                    onClick={submitHandler}
-                                >
-                                    Login
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="AppFormRight position-relative d-flex justify-content-center flex-column align-items-center text-center p-5 text-white">
-                            <h2 class="position-relative px-4 pb-3 mb-4">Welcome Technician</h2>
-                            <p>Technology is best when it brings peoples together!</p>
-                        </div>
+     <div style={style}>
+      <div className="loginContainer go-register">
+        {/* <!-- Login Form Starts --> */}
+        <div className="loginForm__container loginForm__container-login">
+            <h1 className="loginForm__heading">Technician Login</h1>
+            <form action="" className="loginForm">
+                <h3 className="loginForm__heading">Sign In</h3>
+                <p className="loginForm__text">or use your account</p>
+                <input type="email" placeholder="email" className="loginForm__field" ref={emailInputRef}/>
+                <input type="password" placeholder="password" className="loginForm__field" ref={passwordInputRef}/>
+                <div className="row mt-4 mb-4">
+    
+                    <div className="col-md-12">
+                        <Link to="/TechnicianForgotPassword" className="text-muted">
+                            Forgot password?
+                        </Link>
                     </div>
                 </div>
-            </div>
-        </form>
+                <button type="submit" className="loginBtn loginBtn--main" onClick={submitHandler}>
+                    Sign In
+                </button>
+            </form>
+        </div>
+        {/* <!-- Login Form Ends --> */}
+        <div className="loginOverlay-container">
+            {/* <!-- Right Overlay Starts --> */}
+            <div className="loginOverlay loginOverlay--right">
+                <img src='https://img.freepik.com/premium-photo/cute-happy-male-teacher-white-background-ai-generated_971652-235.jpg?size=626&ext=jpg&ga=GA1.1.1085728591.1696533906&semt=ais' alt='' height={250} width={200} style={{borderRadius:"50%"}}/>
+                <h3 className="loginOverlay__heading">Hello Technician</h3>
+                <p className="loginOverlay__desc">Enter your personal details and start journey with us</p>
+            </div>           
+        </div>
+      </div>
     </div>
-</div>
-</div>
     </LayoutTech>
   );
 }
--- src/Componets/pages/Teachnician/techDashboard.js
+++ src/Componets/pages/Teachnician/techDashboard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import LayoutTech from "../../Layout/Layout3";
 import classes from "./techdashboard.module.css"
-import { Button } from "react-bootstrap";
+import './techdashboard.css'
+import { Button , Dropdown } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function TechnicianDashboard() {
     const Navigate = useNavigate();
@@ -19,33 +21,97 @@ function TechnicianDashboard() {
         Navigate("/JobApproval")
         console.log("Successfully Updated");
       };
+      
+      const handleLogout = async () => {
+        const token = Cookies.get('token');
+        console.log("Token from cookies:", token);
+    
+        try {
+            const response = await fetch("http://localhost:3003/api/v1/technician/techLogout", {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                }
+            });
+    
+            const responseData = await response.json(); // assuming server responds with json
+            console.log("Server Response:", responseData);
+    
+            if (response.ok) {
+                Cookies.remove('token');
+                Navigate("/techlogin");
+                console.log("successfully logout");
+            } else {
+                console.log("Logout failed.");
+            }
+        } catch (error) {
+            console.error("There was an error logging out", error);
+        }
+    };
+
+    const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+      <a
+          href=""
+          ref={ref}
+          onClick={(e) => {
+              e.preventDefault();
+              onClick(e);
+          }}
+      >
+          {children}
+      </a>
+  ));
     return(
-        <LayoutTech>
-          <Button variant="secondary"  as= {NavLink} to="/updateTechnicianprofile"> Technician Profile</Button>
-            <div className={classes.cardContainer}>
-              <div className={classes.card1}>
-                <div className={classes.cardBody}>
-                  <div className={classes.cardTitle}>Job Assign</div>
-                  <div className={classes.cardText}>View your Assign Project, you can find more details here.</div>
-                  <button className={classes.cardButton} onClick={JobAssigned} >click Here</button>
-                </div>
+      <LayoutTech>
+      <Dropdown className="imgdropdown">
+                <Dropdown.Toggle as={CustomToggle}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpFdo7jMQ4ZhDD1zqDdGGW0HjKNbV4iiOniQ&usqp=CAU" alt="Profile" style={{ width: '60px', borderRadius: '50%' }} />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item as={NavLink} to="/updateTechnicianprofile">Show Profile</Dropdown.Item>
+                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+      <div className='techdashboard-bodynew'>
+          <div className="techdashboard-blog_post">
+              <div className="techdashboard-img_pod">
+                  <img className="techdashboard-img" src="https://img.freepik.com/free-vector/man-search-hiring-job-online-from-laptop_1150-52728.jpg?w=1380&t=st=1696506931~exp=1696507531~hmac=50499c7a5872e2fc93126b4f2534b7087d0d95d84897b3f64be8542e79e484a6" alt="random image" />
               </div>
-              <div className={classes.card2}>
-                <div className={classes.cardBody}>
-                  <div className={classes.cardTitle}>Job waiting Approval</div>
-                  <div className={classes.cardText}>View Jobs Waiting for Approval,you can see more details here.</div>
+              <div className="techdashboard-container_copy">
+                  <h3 className="techdashboard-h3">Job Assign</h3>
+                  <h1 className="techdashboard-h1">View your Assign Project</h1>
+                  <p className="techdashboard-p">View your Assign Project, you can find more details here.</p>
+                  <button className={classes.cardButton} onClick={JobAssigned}>Click Here</button>
+              </div>
+          </div>
+
+          <div className="techdashboard-blog_post">
+              <div className="techdashboard-img_pod">
+                  <img className="techdashboard-img" src="https://img.freepik.com/free-vector/elegant-businessman-waiting-room_24877-57777.jpg?size=626&ext=jpg&ga=GA1.1.1085728591.1696533906&semt=ais" alt="random image" />
+              </div>
+              <div className="techdashboard-container_copy">
+                  <h3 className="techdashboard-h3">Job waiting Approval</h3>
+                  <h1 className="techdashboard-h1">Jobs Waiting for Approval</h1>
+                  <p className="techdashboard-p">View Jobs Waiting for Approval,you can see more details here.</p>
                   <button className={classes.cardButton} onClick={JobEWaitingAprroval}>Click Here</button>
-                </div>
               </div>
-              <div className={classes.card3}>
-                <div className={classes.cardBody}>
-                  <div className={classes.cardTitle}>Job Closed</div>
-                  <div className={classes.cardText}>View job which are closed, you can see more details here.</div>
+          </div>
+
+          <div className="techdashboard-blog_post">
+              <div className="techdashboard-img_pod">
+                  <img className="techdashboard-img" src="https://img.freepik.com/free-vector/done-concept-illustration_114360-3060.jpg?size=626&ext=jpg&ga=GA1.2.1085728591.1696533906&semt=ais" alt="random image" />
+              </div>
+              <div className="techdashboard-container_copy">
+                  <h3 className="techdashboard-h3">Job Closed</h3>
+                  <h1 className="techdashboard-h1">View closed jobs</h1>
+                  <p className="techdashboard-p">View job which are closed, you can see more details here.</p>
                   <Button variant='Danger' className={classes.cardButton} onClick={JobClosed}>Click Here</Button>
-                </div>
               </div>
-            </div>
-            </LayoutTech>
+          </div>
+      </div>
+  </LayoutTech>
           );
 }
 
