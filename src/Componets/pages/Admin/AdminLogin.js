af9f19b8b727d1cb4cee9e2b09ac2d9d0a4c4a8a
riya-sengar <riyasengar24797@gmail.com>
2023-10-10 17:55:06 +0530

    commit- changes admin login

--- src/Componets/pages/Admin/AdminLogin.js
+++ src/Componets/pages/Admin/AdminLogin.js
@@ -7,7 +7,8 @@ import "./Admin.css"
 import { Link } from "react-router-dom";
 import AdminNavBar from "../../NavBar/AdminNavBar";
 import { Container } from "react-bootstrap";
-import { Card ,Row} from "react-bootstrap";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AdminLogin() {
   const navigate = useNavigate();
@@ -30,7 +31,7 @@ function AdminLogin() {
 
     try {
       const response = await axios.post(
-        "http://localhost:3003/api/v1/companyAdmin/adminLogin",
+        "http://3.110.86.245/api/v1/companyAdmin/adminLogin",
         {
           email: enteredEmail,
           password: enteredPassword,
@@ -50,12 +51,14 @@ function AdminLogin() {
       // You can set an arbitrary expiration time (e.g., 1 hour from now)
       const expirationTime = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
       localStorage.setItem('token',idToken)
+      toast.success("Login successful!");
       console.log(idToken,'id token')
 
       authCtx.login(idToken, expirationTime.toISOString());
       navigate("/AdminD");
     } catch (error) {
-      alert(error.message);
+      console.log(error.message);
+      toast.error(error.message);
     }
   };
   return (
@@ -63,36 +66,40 @@ function AdminLogin() {
       <AdminNavBar/>
       <Container className="container-xxl py-5">
         <Container>
-        <Row xs={1} lg={3} className="g-4">
-
-        </Row>
-          <Card className="service-item rounded pt-3">
-            <Card.Body>
-            <h1 className="AdminloginForm__heading">Admin Login</h1>
+<div style={style}>
+       <div className="AdminloginContainer go-register">
+         {/* <!-- Login Form Starts --> */}
+         <div className="AdminloginForm__container loginForm__container-login">
+             <h1 className="AdminloginForm__heading">Admin Login</h1>
              <form action="" className="AdminloginForm">
                  <h3 className="AdminloginForm__heading">Sign In</h3>
                  <p className="AdminloginForm__text">Or Use Your Account</p>
                  <input type="email" placeholder="Email" className="AdminloginForm__field" ref={emailInputRef} />
                  <input type="password" placeholder="Password" className="AdminloginForm__field" ref={passwordInputRef} />
-                 </form>
-         {/* <!-- Login Form Starts --> */}
-            
+                 <div className="row mt-4 mb-4">
     
+                     <div className="col-md-12">
                          <Link to="/TechnicianForgotPassword" className="Admintext-muted">
                              Forgot password?
                          </Link>
+                     </div>
+                 </div>
                  <button type="submit" className="AdminloginBtn loginBtn--main" onClick={submitHandler}>
                      Sign In
                  </button>
+            </form>
+         </div>
          {/* <!-- Login Form Ends --> */}
+         <div className="Admin-loginOverlay-container">
              {/* <!-- Right Overlay Starts --> */}
+             <div className="AdminloginOverlay loginOverlay--right">
                  <img src='https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80' alt='' height={250} width={200} style={{borderRadius:"50%"}}/>
                  <h3 className="AdminloginOverlay__heading"></h3>
                  <p className="AdminloginOverlay__desc"><h2>Start Your Journey With Us</h2></p>
-            </Card.Body>
-
-          </Card>
-       
+             </div>           
+         </div>
+       </div>
+     </div>
         </Container>
       
       </Container>
--- src/Componets/pages/Admin/adminDash.js
+++ src/Componets/pages/Admin/adminDash.js
@@ -24,46 +24,47 @@ function AdminDashboard() {
 
   return (
     <React.Fragment>
-      <AdminDashboardNavbar/>
-      <Container className="container-xxl py-5">
-        <Container>
-        <Row xs={1} lg={3} className="g-4">
-        <Col lg={6} sm={12} className="wow fadeInUp" data-wow-delay="0.1s" >
-        <Card className="service-item rounded pt-3"> 
-          <Card.Body>
-                  <i className="fa fa-3x fa-globe text-primary mb-4"src="https://img.freepik.com/free-vector/man-search-hiring-job-online-from-laptop_1150-52728.jpg?w=1380&t=st=1696506931~exp=1696507531~hmac=50499c7a5872e2fc93126b4f2534b7087d0d95d84897b3f64be8542e79e484a6" alt="random image" ></i>
-              <Card.Title><h3 className="Admindashboard-h3">Accounts Registered !</h3></Card.Title>  
-                  <h1 className="Admindashboard-h1"></h1>
-                  <Card.Text><p className="Admindashboard-p">View Registered Accounts of Managers !</p></Card.Text>  
-                  <Button onClick={handleRegisteredAccountsClick}>Click Here !!</Button>
-          </Card.Body>
-          
-          </Card>
-        </Col>
-         
-        <Col lg={6} sm={12} className="wow fadeInUp" data-wow-delay="0.3s">
-        <Card  className="service-item rounded pt-3">
-            <Card.Body>
-                            <i className="fa fa-3x fa-globe text-primary mb-4" src="https://img.freepik.com/free-vector/elegant-businessman-waiting-room_24877-57777.jpg?size=626&ext=jpg&ga=GA1.1.1085728591.1696533906&semt=ais" alt="random image" ></i>
-                        <Card.Title>   <h3 className="Admindashboard-h3">Account Waiting Approval !</h3></Card.Title>
-                        <Card.Text> <p className="Admindashboard-p">View Waiting for Approval Accounts of Managers !</p></Card.Text> 
-                            <Button onClick={handleAccountsApprovalClick}>Click Here
-                            !!</Button>
-            </Card.Body>
+    <AdminDashboardNavbar/>
+    <Container className="container-xxl py-5">
+      <Container>
+      <Row xs={1} lg={3} className="g-4">
+      <Col lg={6} sm={12} className="wow fadeInUp" data-wow-delay="0.1s" >
+      <Card className="service-item rounded pt-3"> 
+        <Card.Body>
+                <i className="fa fa-3x fa-globe text-primary mb-4"src="https://img.freepik.com/free-vector/man-search-hiring-job-online-from-laptop_1150-52728.jpg?w=1380&t=st=1696506931~exp=1696507531~hmac=50499c7a5872e2fc93126b4f2534b7087d0d95d84897b3f64be8542e79e484a6" alt="random image" ></i>
+            <Card.Title><h3 className="Admindashboard-h3">Accounts Registered !</h3></Card.Title>  
+                <h1 className="Admindashboard-h1"></h1>
+                <Card.Text><p className="Admindashboard-p">View Registered Accounts of Managers !</p></Card.Text>  
+                <Button onClick={handleRegisteredAccountsClick}>Click Here !!</Button>
+        </Card.Body>
         
-          </Card>
-        </Col>
-         
-         
-
-         
-        </Row>
-        </Container>
+        </Card>
+      </Col>
        
-     
-      </Container>
+      <Col lg={6} sm={12} className="wow fadeInUp" data-wow-delay="0.3s">
+      <Card  className="service-item rounded pt-3">
+          <Card.Body>
+                          <i className="fa fa-3x fa-globe text-primary mb-4" src="https://img.freepik.com/free-vector/elegant-businessman-waiting-room_24877-57777.jpg?size=626&ext=jpg&ga=GA1.1.1085728591.1696533906&semt=ais" alt="random image" ></i>
+                      <Card.Title>   <h3 className="Admindashboard-h3">Account Waiting Approval !</h3></Card.Title>
+                      <Card.Text> <p className="Admindashboard-p">View Waiting for Approval Accounts of Managers !</p></Card.Text> 
+                          <Button onClick={handleAccountsApprovalClick}>Click Here
+                          !!</Button>
+          </Card.Body>
       
- </React.Fragment>
+        </Card>
+      </Col>
+       
+       
+
+       
+      </Row>
+      </Container>
+     
+   
+    </Container>
+    
+</React.Fragment>
+
   );
 }
 
