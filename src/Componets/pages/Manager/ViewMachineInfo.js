dae3aa464ce6f5dc316411393830e09ae89a5c52
anandmatkar <139226044+anandmatkar@users.noreply.github.com>
2023-10-10 17:46:40 +0530

    changes in viewMachineIndo.js

--- src/Componets/pages/Manager/ViewMachineInfo.js
+++ src/Componets/pages/Manager/ViewMachineInfo.js
@@ -40,48 +40,48 @@ export default function ViewMachineInfo() {
 
   return (
     <React.Fragment>
-      <Container>
-        <h1>Machine Info Details</h1>
+        <Container>
+            <h1 className='text-center'>Machine Info Details</h1>        
 
-        <Table bordered hover responsive>
-          <thead>
-            <tr>
-              <th>Order ID</th>
-              <th>Project Type</th>
-              <th>Description</th>
-              <th>Start Date</th>
-              <th>End Date</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {machineInfoDetails.map((machineInfo, i) => (
-              <>
-                <tr key={machineInfo.id}>
-                  <td>{machineInfo.order_id}</td>
-                  <td>{machineInfo.project_type}</td>
-                  <td>{machineInfo.description}</td>
-                  <td>{machineInfo.start_date}</td>
-                  <td>{machineInfo.end_date}</td>
-                  <td>
-                    <Button
-                      variant="success"
-                      size="sm"
-                      onClick={() =>
-                        navigate(
-                          `/project-attached-machine-details/${machineInfo.project_id}`
-                        )
-                      }
-                    >
-                      View Machine's
-                    </Button>
-                  </td>
-                </tr>
-              </>
-            ))}
-          </tbody>
-        </Table>
-      </Container>
+                <Table bordered hover responsive>
+                    <thead>
+                        <tr>
+                        <th>Order ID</th>
+                        <th>Project Type</th>
+                        <th>Description</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            machineInfoDetails.map((machineInfo, i) => (
+                                <>
+                                    <tr key={machineInfo.id}>
+                                        <td>{machineInfo.order_id}</td>
+                                        <td>{machineInfo.project_type}</td>
+                                        <td>{machineInfo.description}</td>
+                                        <td>{machineInfo.start_date}</td>
+                                        <td>{machineInfo.end_date}</td>
+                                        <td>
+                                            <Button
+                                                variant='success'
+                                                size="sm"
+                                                onClick={() => navigate(`/project-attached-machine-details/${machineInfo.project_id}`)}
+                                                
+                                            > 
+                                                View Machine's
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </>
+                            ))
+                        }
+                    </tbody>
+                </Table>
+
+        </Container>
     </React.Fragment>
-  );
+  )
 }
