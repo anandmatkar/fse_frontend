6f6db90bf6eb5f3a0ef64bf3fb1bd5671c3287f5
Vaishnavi Agrawal <vaishnaviagrawal006@gmail.com>
2023-10-17 11:48:34 -0400

    commit- change of regiterd account api

--- src/Componets/pages/Admin/Registeredaccount.js
+++ src/Componets/pages/Admin/Registeredaccount.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Table from 'react-bootstrap/Table';
 import { Base_Url } from '../../../Api/Base_Url';
 import AdminDashboardNavbar from '../../NavBar/AdminDashboardNavbar';
+import Cookies from 'js-cookie';
 
 const Registeredaccount = () => {
 
@@ -40,94 +41,88 @@ const Registeredaccount = () => {
     };
 
     useEffect(() => {
-        const config = {
-            headers: {
-              Authorization: `${localStorage.getItem("token")}`, // Add the token to the headers
-            },
-          };
+      const getCookie = (name) => {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+      }
+  
+      const token = getCookie("token");
+  
+      const config = {
+        headers: {
+          Authorization: token,
+        },
+      };
           
           axios
             .get(`${Base_Url}api/v1/companyAdmin/managerListForApproval`, config)
             .then((response) => {
-              // Assuming the response data is an array of user objects
-              const apiUsers = response.data;
-              console.log(response.data, 'fetcheddata');
-              setUsers(response.data.data.RegisteredManagerList);
-          
-              // Initialize isActive property for each user fetched from the API
-              // const updatedUsers = apiUsers.map((user) => ({
-              //   ...user,
-              //   isActive: false, // You can set the initial value as needed
-              // }));
-              
-              // Now, the GET request will include the authorization token in the headers.
+              setUsers(response.data.data.RegisteredManagerList || []);
             })
             .catch((error) => {
               console.error("Error fetching data:", error);
             })
-    }, []); // Empty dependency array to run the effect only once
+    }, []); 
 
- // Calculate total pages based on the number of items and items per page
-  
+ 
     return (
       <>
-      <AdminDashboardNavbar/>
-      <div className="text-center mb-5 mt-5">
-                    <h6 className="section-title bg-white text-center text-primary px-3">Admin Panel</h6>
-                    <h1>Registered Accounts</h1>
-                </div>
-      <div className="user-table-container">
-        <h1 className='text-center text-info'>Account Registered</h1>
-        <div className='card'>
-          <div className='card-body'>
-          <Table responsive hover >
-          <thead className=''>
-            <tr>
-                <th>Account ID</th>
-                <th>Name</th>
-                <th>Surname</th>
-                <th>Position</th>
-                <th>Company</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              currentUsers.map((currentUsers) => (
-                  <tr key={currentUsers.id}>
-                    <td>{currentUsers.id}</td>
-                    <td>{currentUsers.name}</td>
-                    <td>{currentUsers.surname}</td>
-                    <td>{currentUsers.position}</td>
-                    <td>{currentUsers.company}</td>
-                    <td>{currentUsers.email_address}</td>
-                    <td>{currentUsers.phone_number}</td>
-                    <td>{currentUsers.created_at}</td>
-                  
-                  </tr>
-                ))
-              }
-          </tbody>
-      </Table>
-
-          </div>
+       <AdminDashboardNavbar/>
+        <div className="text-center mb-5 mt-5">
+          <h6 className="section-title bg-white text-center text-primary px-3">Admin Panel</h6>
+          <h1>Registered Accounts</h1>
         </div>
-        <nav className="dt-pagination">
-          <ul className="dt-pagination-ul">
-            <li className={`dt-item ${currentPage === 1 ? 'disabled' : ''}`}>
-              <button className="dt-link" onClick={() => handleChangePage(currentPage - 1)}>Prev</button>
-            </li>
-            {renderPaginationButtons()}
-            <li className={`dt-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-              <button className="dt-link" onClick={() => handleChangePage(currentPage + 1)}>Next</button>
-            </li>
-          </ul>
-        </nav>
-
-              </div>
-              </>
+        <div className="user-table-container">
+          <div className='card'>
+            <div className='card-body'>
+              <Table responsive hover >
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Position</th>
+                    <th>Company</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentUsers && currentUsers.length > 0 ? (
+                    currentUsers.map((user) => (
+                      <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.surname}</td>
+                        <td>{user.position}</td>
+                        <td>{user.company}</td>
+                        <td>{user.email_address}</td>
+                        <td>{user.phone_number}</td>
+                        <td>{user.created_at}</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="7" className="text-center fs-3 fw-bold">No registered accounts</td>
+                    </tr>
+                  )}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+          <nav className="dt-pagination">
+            <ul className="dt-pagination-ul">
+              <li className={`dt-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                <button className="dt-link" onClick={() => handleChangePage(currentPage - 1)}>Prev</button>
+              </li>
+              {renderPaginationButtons()}
+              <li className={`dt-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+                <button className="dt-link" onClick={() => handleChangePage(currentPage + 1)}>Next</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </>
     );
   }
 
