c7a75b2922e8bd3c3995fc57baebaf5d18843ef0
Yash Tiwari <yashtiwari003@gmail.com>
2023-11-24 11:17:39 +0530

    Back Navigations on Admin Panel

--- src/Componets/pages/Admin/AccountW.js
+++ src/Componets/pages/Admin/AccountW.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import './AccountWA.css';
-import Table from 'react-bootstrap/Table';
+import "./AccountWA.css";
+import Table from "react-bootstrap/Table";
 import { Button } from "react-bootstrap";
 import { Base_Url } from "../../../Api/Base_Url";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { FcApprove, FcDisapprove } from 'react-icons/fc'
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FcApprove, FcDisapprove } from "react-icons/fc";
 import AdminDashboardNavbar from "../../NavBar/AdminDashboardNavbar";
 import Cookies from "js-cookie";
 import Spinner from "../Common/Spinner";
 
-
 function UserTable() {
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const handleAction = async (userId, actionType) => {
     const token = Cookies.get("token");
-    setLoading(true); 
+    setLoading(true);
 
     if (!token) {
       console.error("Token not found in localStorage.");
@@ -39,7 +40,7 @@ function UserTable() {
       await axios.put(endPointUrl, null, config);
 
       // Filter out the user from the users array
-      const updatedUsers = users.filter(user => user.id !== userId);
+      const updatedUsers = users.filter((user) => user.id !== userId);
       setUsers(updatedUsers);
 
       if (status === 1) {
@@ -50,113 +51,122 @@ function UserTable() {
     } catch (error) {
       console.error("Error:", error);
     } finally {
-      setLoading(false);  // Stop loading once the process is done, whether successful or not
+      setLoading(false); // Stop loading once the process is done, whether successful or not
     }
-
   };
 
-
   useEffect(() => {
-    const token= Cookies.get('token')
-      const config = {
-          headers: {
-            Authorization: token, // Add the token to the headers
-          },
-        };
+    const token = Cookies.get("token");
+    const config = {
+      headers: {
+        Authorization: token, // Add the token to the headers
+      },
+    };
     axios
-    .get(`${Base_Url}api/v1/companyAdmin/managerListForApproval`, config)
-    .then((response) => {
-      if (response.data.success === false) {
-        toast.error(response.data.message);
-        setUsers([]); // set to empty array so you can display the "No manager for approval" message
-      } else {
-        setUsers(response.data.data.ManagerListForApproval || []);
-      }
-      setLoading(false);
-    })
-    .catch((error) => {
-      if (error.response && error.response.status === 401) {
-        console.error("Unauthorized access:", error);
-      } else {
-        console.error("Error:", error);
-      }
-    });
+      .get(`${Base_Url}api/v1/companyAdmin/managerListForApproval`, config)
+      .then((response) => {
+        if (response.data.success === false) {
+          toast.error(response.data.message);
+          setUsers([]); // set to empty array so you can display the "No manager for approval" message
+        } else {
+          setUsers(response.data.data.ManagerListForApproval || []);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          console.error("Unauthorized access:", error);
+        } else {
+          console.error("Error:", error);
+        }
+      });
   }, []);
 
   return (
     <>
-    
-    <AdminDashboardNavbar/>
-    
-    <div className="text-center mb-5 mt-5">
-  <h6 className="section-title bg-white text-center text-primary px-3">Admin Panel</h6>
-  <h1>Accounts Waiting for Approval</h1>
-</div>
-{loading ? (
+      <AdminDashboardNavbar />
+
+      <div className="text-center mb-5 mt-5">
+        <h6 className="section-title bg-white text-center text-primary px-3">
+          Admin Panel
+        </h6>
+        <h1>Accounts Waiting for Approval</h1>
+      </div>
+
+      {loading ? (
         <Spinner />
       ) : (
-<div className="user-table-container">
-  <div className="card">
-    <div className="card-body">
-      <Table responsive hover>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Position</th>
-            <th>Company</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users && users.length > 0 ? (
-            users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.name}</td>
-                <td>{user.surname}</td>
-                <td>{user.position}</td>
-                <td>{user.company}</td>
-                <td>{user.email_address}</td>
-                <td>{user.phone_number}</td>
-                <td>{user.created_at}</td>
-                <td>
-                  <Button
-                    variant="primary"
-                    disabled={user.status === 1}
-                    onClick={() => handleAction(user.id, "approve")} 
-                    style={{ marginRight: '10px' }}
-                    className="button-hover-effect">
-                    <FcApprove size={34} />
-                  </Button>
+        <div className="user-table-container">
+          <Button
+            variant="primary"
+            onClick={() => navigate("/AdminD")}
+            className="my-2"
+          >
+            Back to Admin Dashboard
+          </Button>
 
-                  <Button
-                    variant={user.status === 1 ? "secondary" : "danger"}
-                    disabled={user.status === 1}
-                    onClick={() => handleAction(user.id, "reject")} 
-                    className="button-hover-effect">
-                    <FcDisapprove size={34} />
-                  </Button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="8" className="text-center fw-bold mt-4">
-                <h3>No manager for approval</h3>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-    </div>
-  </div>
-</div>
-  )}
+          <div className="card">
+            <div className="card-body">
+              <Table responsive hover>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Position</th>
+                    <th>Company</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Date</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users && users.length > 0 ? (
+                    users.map((user) => (
+                      <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.surname}</td>
+                        <td>{user.position}</td>
+                        <td>{user.company}</td>
+                        <td>{user.email_address}</td>
+                        <td>{user.phone_number}</td>
+                        <td>{user.created_at}</td>
+                        <td>
+                          <Button
+                            variant="primary"
+                            disabled={user.status === 1}
+                            onClick={() => handleAction(user.id, "approve")}
+                            style={{ marginRight: "10px" }}
+                            className="button-hover-effect"
+                          >
+                            <FcApprove size={34} />
+                          </Button>
+
+                          <Button
+                            variant={user.status === 1 ? "secondary" : "danger"}
+                            disabled={user.status === 1}
+                            onClick={() => handleAction(user.id, "reject")}
+                            className="button-hover-effect"
+                          >
+                            <FcDisapprove size={34} />
+                          </Button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="8" className="text-center fw-bold mt-4">
+                        <h3>No manager for approval</h3>
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+      )}
     </>
-    
   );
 }
 
--- src/Componets/pages/Admin/Registeredaccount.js
+++ src/Componets/pages/Admin/Registeredaccount.js
@@ -1,111 +1,141 @@
-import React ,{useEffect,useState}from 'react'
-import axios from 'axios'
-import Table from 'react-bootstrap/Table';
-import { Base_Url } from '../../../Api/Base_Url';
-import AdminDashboardNavbar from '../../NavBar/AdminDashboardNavbar';
-import Cookies from 'js-cookie';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Button, Table } from "react-bootstrap";
+import { Base_Url } from "../../../Api/Base_Url";
+import AdminDashboardNavbar from "../../NavBar/AdminDashboardNavbar";
+import Cookies from "js-cookie";
+
 const Registeredaccount = () => {
-    const [users, setUsers] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 10;
-    const toggleActivation = (userId) => {
-      setUsers((prevUsers) =>
-        prevUsers.map((user) =>
-          user.id === userId ? { ...user, isActive: !user.isActive } : user
-        )
-      );
-    };
-    const totalPages = Math.ceil(users.length / itemsPerPage);
-    const lastIndex = currentPage * itemsPerPage;
-    const firstIndex = lastIndex - itemsPerPage;
-    const currentUsers = users.slice(firstIndex, lastIndex);
-    const handleChangePage = (newPage) => {
-      setCurrentPage(newPage);
-    };
-    const renderPaginationButtons = () => {
-      const buttons = [];
-      for (let i = 1; i <= totalPages; i++) {
-        buttons.push(
-          <li key={i} className={`dt-item ${currentPage === i ? 'active' : ''}`}>
-            <button className="dt-link" onClick={() => handleChangePage(i)}>{i}</button>
-          </li>
-        );
-      }
-      return buttons;
-    };
-    useEffect(() => {
-      const token= Cookies.get('token')
-      const config = {
-        headers: {
-          Authorization: token,
-        },
-      };
-          axios
-            .get(`${Base_Url}api/v1/companyAdmin/managerListForApproval`, config)
-            .then((response) => {
-              setUsers(response.data.data.RegisteredManagerList || []);
-            })
-            .catch((error) => {
-              console.error("Error fetching data:", error);
-            })
-    }, []);
-    return (
-      <>
-       <AdminDashboardNavbar/>
-        <div className="text-center mb-5 mt-5">
-          <h6 className="section-title bg-white text-center text-primary px-3">Admin Panel</h6>
-          <h1>Registered Accounts</h1>
-        </div>
-        <div className="user-table-container">
-          <div className='card'>
-            <div className='card-body'>
-              <Table responsive hover >
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Surname</th>
-                    <th>Position</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {currentUsers && currentUsers.length > 0 ? (
-                    currentUsers.map((user) => (
-                      <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.surname}</td>
-                        <td>{user.position}</td>
-                        <td>{user.company}</td>
-                        <td>{user.email_address}</td>
-                        <td>{user.phone_number}</td>
-                        <td>{user.created_at}</td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan="7" className="text-center fs-3 fw-bold">No registered accounts</td>
-                    </tr>
-                  )}
-                </tbody>
-              </Table>
-            </div>
-          </div>
-          <nav className="dt-pagination">
-            <ul className="dt-pagination-ul">
-              <li className={`dt-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                <button className="dt-link" onClick={() => handleChangePage(currentPage - 1)}>Prev</button>
-              </li>
-              {renderPaginationButtons()}
-              <li className={`dt-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-                <button className="dt-link" onClick={() => handleChangePage(currentPage + 1)}>Next</button>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </>
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const toggleActivation = (userId) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === userId ? { ...user, isActive: !user.isActive } : user
+      )
     );
-  }
-export default Registeredaccount
+  };
+  const totalPages = Math.ceil(users.length / itemsPerPage);
+  const lastIndex = currentPage * itemsPerPage;
+  const firstIndex = lastIndex - itemsPerPage;
+  const currentUsers = users.slice(firstIndex, lastIndex);
+  const handleChangePage = (newPage) => {
+    setCurrentPage(newPage);
+  };
+  const renderPaginationButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <li key={i} className={`dt-item ${currentPage === i ? "active" : ""}`}>
+          <button className="dt-link" onClick={() => handleChangePage(i)}>
+            {i}
+          </button>
+        </li>
+      );
+    }
+    return buttons;
+  };
+  useEffect(() => {
+    const token = Cookies.get("token");
+    const config = {
+      headers: {
+        Authorization: token,
+      },
+    };
+    axios
+      .get(`${Base_Url}api/v1/companyAdmin/managerListForApproval`, config)
+      .then((response) => {
+        setUsers(response.data.data.RegisteredManagerList || []);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  return (
+    <>
+      <AdminDashboardNavbar />
+      <div className="text-center mb-5 mt-5">
+        <h6 className="section-title bg-white text-center text-primary px-3">
+          Admin Panel
+        </h6>
+        <h1>Registered Accounts</h1>
+      </div>
+      <div className="user-table-container">
+        <Button
+          variant="primary"
+          onClick={() => navigate("/AdminD")}
+          className="my-2"
+        >
+          Back to Admin Dashboard
+        </Button>
+        <div className="card">
+          <div className="card-body">
+            <Table responsive hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Surname</th>
+                  <th>Position</th>
+                  <th>Company</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentUsers && currentUsers.length > 0 ? (
+                  currentUsers.map((user) => (
+                    <tr key={user.id}>
+                      <td>{user.name}</td>
+                      <td>{user.surname}</td>
+                      <td>{user.position}</td>
+                      <td>{user.company}</td>
+                      <td>{user.email_address}</td>
+                      <td>{user.phone_number}</td>
+                      <td>{user.created_at}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="7" className="text-center fs-3 fw-bold">
+                      No registered accounts
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+        <nav className="dt-pagination">
+          <ul className="dt-pagination-ul">
+            <li className={`dt-item ${currentPage === 1 ? "disabled" : ""}`}>
+              <button
+                className="dt-link"
+                onClick={() => handleChangePage(currentPage - 1)}
+              >
+                Prev
+              </button>
+            </li>
+            {renderPaginationButtons()}
+            <li
+              className={`dt-item ${
+                currentPage === totalPages ? "disabled" : ""
+              }`}
+            >
+              <button
+                className="dt-link"
+                onClick={() => handleChangePage(currentPage + 1)}
+              >
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+export default Registeredaccount;
