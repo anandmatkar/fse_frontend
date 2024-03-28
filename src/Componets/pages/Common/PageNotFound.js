c4f27ad5463f887994e2ae802eeee950a3fe3523
Yash Tiwari <yashtiwari003@gmail.com>
2024-03-28 11:23:55 +0530

    Changes - Remove Unused Dep

---
+++ src/Componets/pages/Common/PageNotFound.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Layout from "../../Layout/Layout";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "./LoginMain.css";
+
+import PageSpinner from "./PageSpinner";
+
+function PageNotFound() {
+  return (
+    <Layout>
+      <Container className="admin-container-main container-xxl py-5 mt-5">
+        <Container>
+          <div
+            className="admin-data text-center wow fadeInUp"
+            data-wow-delay="0.1s"
+          >
+            <h6 className="section-title bg-white text-center text-primary px-3">
+              PageNotFound
+            </h6>
+            <h1 className="mb-5">PageNotFound</h1>
+          </div>
+        </Container>
+      </Container>
+    </Layout>
+  );
+}
+
+export default PageNotFound;
